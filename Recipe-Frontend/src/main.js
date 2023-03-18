27ec74567aeafef2265e443a5d8a08f953cf94cd
KINGD4LAL <hamzahdalal12@gmail.com>
2023-03-18 02:12:50 +0000

    Starter_Code

---
+++ Recipe-Frontend/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from '../src/views/App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
+import 'jquery'
+import 'popper.js'
+import router from "./router"
+
+const app = createApp(App)
+
+app.use(router).mount('#app')
---
+++ Recipe-Frontend/src/views/components/button_test.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="btn-group">
+  <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+    Action
+    <span class="caret"></span>
+  </a>
+  <ul class="dropdown-menu">
+    <!-- dropdown menu links -->
+  </ul>
+</div>
+</template>
+<script>
+</script>
