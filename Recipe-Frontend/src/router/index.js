201e2ee60aca25fb5c259162a57fe44fb801aadc
MA1002643 <87866666+MA1002643@users.noreply.github.com>
2023-03-19 02:25:35 +0000

    Creating NavBar

--- Recipe-Frontend/src/router/index.js
+++ Recipe-Frontend/src/router/index.js
@@ -1,14 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import Home from "../views/pages/Home.vue"
+import Home from "../views/pages/Home.vue";
+import Feed from "../views/pages/Feed.vue";
+import Dashboard from "../views/pages/Dashboard.vue";
 
 const routes = [
-    { path: "/", component: Home}
-]
+  { path: "/", component: Home },
+  { path: "/feed", component: Feed },
+  { path: "/dashboard", component: Dashboard },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-})
+  history: createWebHistory(),
+  routes,
+});
 
 export default router;
--- Recipe-Frontend/src/views/App.vue
+++ Recipe-Frontend/src/views/App.vue
@@ -1,3 +1,42 @@
 <template>
-    <router-view />
+  <nav class="navbar">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" style="color: aqua" href="#">Recepie finder</a>
+      </div>
+      <router-link
+        class="navbar-link"
+        style="text-decoration: none"
+        to="/dashboard"
+        >Dashboard</router-link
+      >
+      <router-link class="navbar-link" style="text-decoration: none" to="/feed"
+        >Feed</router-link
+      >
+      <router-link class="navbar-link" style="text-decoration: none" to="/"
+        >Home</router-link
+      >
+      <div class="nav navbar-nav navbar-right">
+        <router-link
+          class="navbar-link"
+          style="text-decoration: none"
+          to="/login"
+          >Login</router-link
+        >
+      </div>
+    </div>
+  </nav>
+
+  <router-view />
 </template>
+<style>
+.navbar {
+  background: #141e30;
+  background: -webkit-linear-gradient(to right, #243b55, #141e30);
+  background: linear-gradient(to right, #243b55, #141e30);
+}
+
+.navbar-link {
+  color: aqua;
+}
+</style>
