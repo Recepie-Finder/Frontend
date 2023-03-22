b2427fdce57e384e7d33d6bcca452ec619d3a61e
MA1002643 <87866666+MA1002643@users.noreply.github.com>
2023-03-22 02:13:14 +0000

    Signup page

--- Recipe-Frontend/src/views/pages/Signup.vue
+++ Recipe-Frontend/src/views/pages/Signup.vue
@@ -1,31 +1,38 @@
 <template>
-  <section class="vh-100 gradient-custom">
+  <section class="h-100 bg-dark">
     <div class="container py-5 h-100">
-      <div class="row justify-content-center align-items-center h-100">
-        <div class="col-12 col-lg-6 col-xl-5">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col">
           <div
-            class="card"
+            class="card card-registration my-4"
             style="border-radius: 1rem; background-color: #146166"
           >
-            <div
-              class="card-body p-5 text-center W-100"
-              style="box-shadow: 20px 20px 20px 20px #a8c7ae"
-            >
-              <div class="mb-md-5 mt-md-4 pb-5">
-                <h2 class="fw-bold mb-2 text-uppercase">Sign up</h2>
-                <p class="text-white-50 mb-5">Please register here!</p>
+            <div class="row g-0">
+              <div class="col-xl-6 d-none d-xl-block">
+                <img
+                  src="https://hips.hearstapps.com/hmg-prod/images/delish-190814-mcdonalds-apple-pies-0283-portrait-pf-1566604978.jpg"
+                  alt="Sample photo"
+                  class="img-fluid"
+                  style="
+                    border-top-left-radius: 0.25rem;
+                    border-bottom-left-radius: 0.25rem;
+                  "
+                />
+              </div>
+              <div class="col-xl-6">
+                <div class="card-body p-md-5 text-black">
+                  <h3 class="mb-5 text-uppercase">RF Registration Form</h3>
 
-                <form @submit.prevent="handleSubmit">
                   <div class="row">
                     <div class="col-md-6 mb-4">
                       <div class="form-outline">
                         <input
                           type="text"
-                          id="firstName"
+                          id="form3Example1m"
                           class="form-control form-control-lg"
                         />
-                        <label class="form-label" for="firstName"
-                          >First Name</label
+                        <label class="form-label" for="form3Example1m"
+                          >First name</label
                         >
                       </div>
                     </div>
@@ -33,66 +40,58 @@
                       <div class="form-outline">
                         <input
                           type="text"
-                          id="lastName"
+                          id="form3Example1n"
                           class="form-control form-control-lg"
                         />
-                        <label class="form-label" for="lastName"
-                          >Last Name</label
+                        <label class="form-label" for="form3Example1n"
+                          >Last name</label
                         >
                       </div>
                     </div>
                   </div>
-                  <div class="form-outline form-white mb-4">
+
+                  <div class="form-outline mb-4">
                     <input
-                      type="email"
-                      id="typeEmailX"
+                      type="text"
+                      id="form3Example8"
                       class="form-control form-control-lg"
-                      placeholder="Email"
-                      name="email"
-                      v-model="email"
                     />
-                    <label class="form-label" for="typeEmailX"></label>
-                    <div v-show="submitted && !email">
-                      This is a required field
-                    </div>
+                    <label class="form-label" for="form3Example8"
+                      >Email Address</label
+                    >
                   </div>
 
-                  <div class="form-outline form-white mb-4">
+                  <div class="form-outline mb-4">
                     <input
-                      type="password"
-                      id="typePasswordX"
+                      type="text"
+                      id="form3Example9"
                       class="form-control form-control-lg"
-                      placeholder="Password"
-                      name="Password"
-                      v-model="password"
                     />
-                    <label class="form-label" for="typePasswordX"></label>
-                    <div v-show="submitted && !password">
-                      This is a required field
-                    </div>
+                    <label class="form-label" for="form3Example9"
+                      >Password</label
+                    >
                   </div>
 
-                  <p class="small mb-5 pb-lg-2">
-                    <a class="text-white-50" href="#!">Forgot password?</a>
-                  </p>
+                  <div class="form-outline mb-4">
+                    <input
+                      type="text"
+                      id="form3Example90"
+                      class="form-control form-control-lg"
+                    />
+                    <label class="form-label" for="form3Example90"
+                      >Confirm Password</label
+                    >
+                  </div>
 
-                  <button
-                    class="btn btn-outline-light btn-lg px-5"
-                    type="submit"
-                  >
-                    Login
-                  </button>
-                  <div v-if="error">{{ error }}</div>
-                </form>
-              </div>
-
-              <div>
-                <p class="mb-0">
-                  Already have an account
-                  <router-link :to="{ path: '/login' }"
-                    ><a>Login</a></router-link
-                  >
-                </p>
+                  <div class="d-flex justify-content-end pt-3">
+                    <button type="button" class="btn btn-light btn-lg">
+                      Reset all
+                    </button>
+                    <button type="button" class="btn btn-warning btn-lg ms-2">
+                      Submit form
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -120,7 +119,7 @@ export default {
       usersService
         .login(this.email, this.password)
         .then((result) => {
-          this.$router.push("/dashboard");
+          this.$router.push("/login");
         })
         .catch((error) => {
           this.error = error;
@@ -131,10 +130,6 @@ export default {
 };
 </script>
 <style>
-.container {
-  width: 100%;
-}
-
 .form {
   margin-top: 120px;
 }
